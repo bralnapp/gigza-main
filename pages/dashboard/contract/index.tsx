f3eaf0b0cdae0cf9d6117e90af1be11fd2c96ce4
coolpythoncodes <coolpythoncodes@gmail.com>
2023-01-22 13:35:28 +0100

    list of active,pending and closed contracts

---
+++ pages/dashboard/contract/index.tsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import DashboardLayout from "@/modules/dashboard/components/layout";
+import { contracts } from "utils/data";
+import Image from "next/image";
+import useWindowSize from "utils/hooks/useWindowSize.hook";
+
+// images
+import squareDot from "@/public/asset/icons/square-dot.svg";
+import redDot from "@/public/asset/icons/red-dot.svg";
+
+type ContractTypes = "active" | "closed" | "pending";
+
+const Contract = () => {
+	const sections = ["active", "closed", "pending"];
+	const [activeSection, setActiveSection] = useState<ContractTypes>(
+		sections[0] as ContractTypes
+	);
+	const { width } = useWindowSize();
+	return (
+		<DashboardLayout>
+			<div className="dashboard-layout-container pt-8 min-[540px]:pt-[42px] pb-[95px]">
+				<h1 className="capitalize font-bold text-xl md:text-[24px] leading-6 md:leading-[29px] text-b1">
+					Contracts
+				</h1>
+
+				<div className="flex items-center mt-6 mb-7">
+					{sections.map((item, index) => (
+						<div
+							key={index}
+							className={`text-sm min-[540px]:text-base leading-[18px] font-bold capitalize py-2 px-4 md:pb-6 cursor-pointer ${
+								activeSection === item
+									? "border-b-2 border-primary text-primary"
+									: "text-b4"
+							} ${item.toLowerCase() === 'pending' ? 'flex items-start gap-x-2' : null}`}
+							onClick={() => setActiveSection(sections[index] as ContractTypes)}
+						>
+							{item.toLowerCase() === "pending" ? (
+								<>
+									{item} <Image src={redDot} alt="" />
+								</>
+							) : (
+								<>
+									{item} {width! < 1024 ? null : "contracts"}
+								</>
+							)}
+						</div>
+					))}
+				</div>
+
+				<div className="mt-4">
+					{contracts[activeSection]?.map((item, index) => (
+						<div
+							key={`contracts-${index}`}
+							className="py-4 md:py-6 px-3 md:px-5 rounded-lg md:rounded-[6px]"
+						>
+							<div className="md:mb-[13px] md:grid grid-flow-col gap-x-[55px]">
+								<div className="">
+									<h3 className="font-bold text-b1 text-base min-[540px]:text-lg leading-[19px] min-[540px]:leading-[22px]">
+										{item?.title}
+									</h3>
+									<p className="py-[15px] md:py-0 md:pt-2 text-b4 text-sm min-[540px]:text-base leading-[21px] min-[540px]:leading-6">
+										{item?.description}
+									</p>
+								</div>
+								<p className="text-sm leading-[19px] text-b4 capitalize ml-auto">
+									{activeSection === "active" ? (
+										`End Date: ${item?.endDate}`
+									) : (
+										<p
+											className={`capitalize text-[10px] min-[540px]:text-[13px] leading-4 rounded-3xl w-fit py-[5px] min-[540px]:py-2 px-[10px] min-[540px]:px-4 ${
+												item?.status.toLowerCase() === "completed"
+													? "bg-[rgba(149,214,164,0.2)] text-[#0E9802]  "
+													: "bg-[rgba(255,184,0,0.1)] text-[#FFB800]"
+											}`}
+										>
+											{item?.status}
+										</p>
+									)}
+								</p>
+							</div>
+							<div className="flex items-center space-x-2 capitalize text-b1 text-sm leading-[17px] mt-[15px]">
+								<p>{item?.name}</p>
+								<Image src={squareDot} alt="" />
+								<p>initiated {item?.startDate}</p>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</DashboardLayout>
+	);
+};
+
+export default Contract;
--- utils/data/dummy.data.ts
+++ utils/data/dummy.data.ts
@@ -39,38 +39,141 @@ export const recentJobs = [
 
 export const sentProposals = [
 	{
-        title: 'Freelance UI/UX Designer (Mobile app)',
-        description: 'I work for a tech start-up called MODCLUB, and we are looking to have our 15-page investor deck refreshed by a professional designer. We are looking for a clean and simple final product that is not overly designed. Timeline- asap.',
-        name: 'Roger Siphron',
-        date: 'Jun 17, 2022',
-        status: 'open',
-    },
-    {
-        title: 'UX/UI Designer for cryptocurrency gambling website.',
-        description: "I'm looking for someone who can handle a logo designing project. If you are interested, kindly send a message.",
-        name: 'Chance Donin',
-        date: 'Jun 17, 2022',
-        status: 'closed',
-    },
-    {
-        title: 'Design the UI for an online B2C health platform',
-        description: "Hi, I'm looking for a designer to help me create 20 screen UI designs for Android and iOS. Kindly send your portfolio and your quote for this job. Thanks!",
-        name: 'Maria Dorwart',
-        date: 'Jun 17, 2022',
-        status: 'closed',
-    },
-    {
-        title: 'Website in webflow',
-        description: "We have a branding package and need a website built in webflow. Looking for someone with a design eye who can help make our branding package come to life.",
-        name: 'Tiana Dias',
-        date: 'Jun 17, 2022',
-        status: 'open',
-    },
-    {
-        title: 'Tech company pitch deck',
-        description: "I work for a tech start-up called MODCLUB, and we are looking to have our 15-page investor deck refreshed by a professional designer. We are looking for a clean and simple final product that is not overly designed. Timeline- asap.",
-        name: 'Cooper Kenter',
-        date: 'Jun 17, 2022',
-        status: 'closed',
-    },
-]
+		title: "Freelance UI/UX Designer (Mobile app)",
+		description:
+			"I work for a tech start-up called MODCLUB, and we are looking to have our 15-page investor deck refreshed by a professional designer. We are looking for a clean and simple final product that is not overly designed. Timeline- asap.",
+		name: "Roger Siphron",
+		date: "Jun 17, 2022",
+		status: "open"
+	},
+	{
+		title: "UX/UI Designer for cryptocurrency gambling website.",
+		description:
+			"I'm looking for someone who can handle a logo designing project. If you are interested, kindly send a message.",
+		name: "Chance Donin",
+		date: "Jun 17, 2022",
+		status: "closed"
+	},
+	{
+		title: "Design the UI for an online B2C health platform",
+		description:
+			"Hi, I'm looking for a designer to help me create 20 screen UI designs for Android and iOS. Kindly send your portfolio and your quote for this job. Thanks!",
+		name: "Maria Dorwart",
+		date: "Jun 17, 2022",
+		status: "closed"
+	},
+	{
+		title: "Website in webflow",
+		description:
+			"We have a branding package and need a website built in webflow. Looking for someone with a design eye who can help make our branding package come to life.",
+		name: "Tiana Dias",
+		date: "Jun 17, 2022",
+		status: "open"
+	},
+	{
+		title: "Tech company pitch deck",
+		description:
+			"I work for a tech start-up called MODCLUB, and we are looking to have our 15-page investor deck refreshed by a professional designer. We are looking for a clean and simple final product that is not overly designed. Timeline- asap.",
+		name: "Cooper Kenter",
+		date: "Jun 17, 2022",
+		status: "closed"
+	}
+];
+
+export const contracts = {
+	active: [
+		{
+			title: "Tech company pitch deck",
+			description:
+				"I work for a tech start-up called MODCLUB, and we are looking to have our 15-page investor deck refreshed by a professional designer. We are looking for a clean and simple final product that is not overly designed. Timeline- asap.",
+			endDate: "Sept 21, 2022",
+			startDate: "Jun 17, 2022",
+			name: "Davis Herwitz"
+		},
+		{
+			title: "Graphics designs",
+			description:
+				"I'm looking for someone who can handle a logo designing project. If you are interested, kindly send a message.",
+			endDate: "Sept 21, 2022",
+			startDate: "Jun 17, 2022",
+			name: "Leo Torff"
+		},
+		{
+			title: "UI app design",
+			description:
+				"Hi, I'm looking for a designer to help me create 20 screen UI designs for Android and iOS. Kindly send your portfolio and your quote for this job. Thanks!",
+			endDate: "Sept 21, 2022",
+			startDate: "Jun 17, 2022",
+			name: "Martin Vaccaro"
+		},
+		{
+			title: "Website in webflow",
+			description:
+				"We have a branding package and need a website built in webflow. Looking for someone with a design eye who can help make our branding package come to life.",
+			endDate: "Sept 21, 2022",
+			startDate: "Jun 17, 2022",
+			name: "Alfonso Botosh"
+		},
+		{
+			title: "Tech company pitch deck",
+			description:
+				"I work for a tech start-up called MODCLUB, and we are looking to have our 15-page investor deck refreshed by a professional designer. We are looking for a clean and simple final product that is not overly designed. Timeline- asap.",
+			endDate: "Sept 21, 2022",
+			startDate: "Jun 17, 2022",
+			name: "Kadin Baptista"
+		}
+	],
+	closed: [
+		{
+			title: "Basic Webpage and Multifunctional MVP wireframe",
+			description:
+				"I work for a tech start-up called MODCLUB, and we are looking to have our 15-page investor deck refreshed by a professional designer. We are looking for a clean and simple final product that is not overly designed. Timeline- asap.",
+			status: "awaiting payment",
+			name: "raphael Benjamin",
+			startDate: "Jun 17, 2022"
+		},
+		{
+			title: "UX/UI designer - Long term",
+			description:
+				"I'm looking for someone who can handle a logo designing project. If you are interested, kindly send a message.",
+			status: "completed",
+			name: "raphael Benjamin",
+			startDate: "Jun 17, 2022"
+		},
+		{
+			title: "UI app design",
+			description:
+				"Hi, I'm looking for a designer to help me create 20 screen UI designs for Android and iOS. Kindly send your portfolio and your quote for this job. Thanks!",
+			status: "completed",
+			name: "raphael Benjamin",
+			startDate: "Jun 17, 2022"
+		},
+		{
+			title: "Website in webflow",
+			description:
+				"We have a branding package and need a website built in webflow. Looking for someone with a design eye who can help make our branding package come to life.",
+			status: "completed",
+			name: "raphael Benjamin",
+			startDate: "Jun 17, 2022"
+		},
+		{
+			title: "Tech company pitch deck",
+			description:
+				"I work for a tech start-up called MODCLUB, and we are looking to have our 15-page investor deck refreshed by a professional designer. We are looking for a clean and simple final product that is not overly designed. Timeline- asap.",
+			status: "completed",
+			name: "raphael Benjamin",
+			startDate: "Jun 17, 2022"
+		}
+	],
+	pending: [
+		{
+			title: "Tech company pitch deck",
+			description:
+				"I work for a tech start-up called MODCLUB, and we are looking to have our 15-page investor deck refreshed by a professional designer. We are looking for a clean and simple final product that is not overly designed. Timeline- asap.",
+			status: "pending",
+			name: "raphael Benjamin",
+			startDate: "Jun 17, 2022"
+
+		}
+	]
+};
