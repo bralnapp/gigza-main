91618961608ad87d62a73fa41b03225c56dfadd1
coolpythoncodes <coolpythoncodes@gmail.com>
2023-01-31 05:00:17 +0100

    landing page corrections

--- utils/data/partners-logos.data.ts
+++ utils/data/partners-logos.data.ts
@@ -3,14 +3,21 @@ import hubspot from "@/public/asset/partners logo/hubspot.svg";
 import google from "@/public/asset/partners logo/google.svg";
 import microsoft from "@/public/asset/partners logo/microsoft.svg";
 import walmart from "@/public/asset/partners logo/walmart.svg";
+import binance from "@/public/asset/partners logo/binance.svg";
+import coinbase from "@/public/asset/partners logo/coinbase.svg";
+import uniswap from "@/public/asset/partners logo/uniswap.svg";
+
 // import fedex from "@/public/asset/partners logo/fedex.svg";
 
 export const partnerLogos = [
+	binance,
 	airbnb,
+	coinbase,
 	hubspot,
 	google,
+	uniswap,
 	microsoft,
 	walmart,
-	google,
+	google
 	// fedex
 ];
