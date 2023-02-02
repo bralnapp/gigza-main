2f0f99a54b7c6d88b8a879e1ee78edc288457781
coolpythoncodes <coolpythoncodes@gmail.com>
2023-02-02 21:06:07 +0100

    get user Dai balance

--- modules/dashboard/sections/wallet/wallet-balance.tsx
+++ modules/dashboard/sections/wallet/wallet-balance.tsx
@@ -1,11 +1,23 @@
+import numeral from "numeral";
+import { DaiContractAbi, DiaContractAddress, formatUnit } from "utils/helper";
+import { useAccount, useContractRead } from "wagmi";
+
 const WalletBalance = () => {
+	const { address } = useAccount();
+	const { data: balance } = useContractRead({
+		address: DiaContractAddress,
+		abi: DaiContractAbi,
+		functionName: "balanceOf",
+		args: [address]
+	});
+	console.log("bal", balance);
 	return (
-		<div className="bg-white rounded-lg p-4 min-[540px]:py-8 min-[540px]:px-6 mb-[22px]">
-			<p className="mb-1 text-b4 text-sm min-[540px]:text-[18px] leading-[17px] min-[540px]:leading-[22px] capitalize">
+		<div className="mb-[22px] rounded-lg bg-white p-4 min-[540px]:py-8 min-[540px]:px-6">
+			<p className="mb-1 text-sm capitalize leading-[17px] text-b4 min-[540px]:text-[18px] min-[540px]:leading-[22px]">
 				wallet Balance
 			</p>
-			<h3 className="font-bold text-2xl min-[540px]:text-4xl leading-[29px] min-[540px]:leading-[43px]">
-				$1000.46
+			<h3 className="text-2xl font-bold leading-[29px] min-[540px]:text-4xl min-[540px]:leading-[43px]">
+				DAI {numeral(formatUnit(balance)).format(",") || 0}
 			</h3>
 		</div>
 	);
