6cffce112fdd09ea4097944e1edbad0cedb6e1a7
coolpythoncodes <coolpythoncodes@gmail.com>
2023-03-06 06:33:15 +0100

    working on notification

--- modules/dashboard/components/notification-items/notification-bell.tsx
+++ modules/dashboard/components/notification-items/notification-bell.tsx
@@ -1,5 +1,5 @@
 import NotificationItem from ".";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NotificationPopOver from "./notification-popOver";
 
 // images
@@ -11,25 +11,29 @@ import {
 	formatUnit,
 	formatWalletAddress,
 	GigzaContractAbi,
-	GigzaContractAddress
+	GigzaContractAddress,
+	useGetPersistedNotificationStore,
+	useSetPersistNotificationStore
 } from "utils/helper";
 import numeral from "numeral";
 
-// const useGetPersistedStore = () => {
-// 	if (
-// 		typeof window !== "undefined" &&
-// 		localStorage.getItem("persist-gigza-notification") !== "undefined"
-// 	) {
-// 		// @ts-ignore
-// 		JSON.parse(localStorage.getItem("persist-gigza-notification")) || [];
-// 	}
-// };
+type Notification = {
+	userAddress: `0x${string}` | undefined;
+	notification: string[];
+}[];
 
 const NotificationBell = () => {
-	const [showNotification, setShowNotification] = useState(false);
-	const [notifications, setNotifications] = useState<string[]>([]);
 	const { address } = useAccount();
+
+	const [showNotification, setShowNotification] = useState(false);
+	const [notifications, setNotifications] = useState<Notification>(
+		useGetPersistedNotificationStore()
+	);
+	useSetPersistNotificationStore(notifications, address!);
+
 	const handleClick = () => setShowNotification(!showNotification);
+
+	console.log("notifications", notifications);
 
 	// ------------------------------ Transfer event (mint DAI tokens) ----------------------------
 
@@ -38,13 +42,30 @@ const NotificationBell = () => {
 		abi: DaiContractAbi,
 		eventName: "Transfer",
 		listener(from, to, value) {
-			if (to === address)
-				setNotifications([
-					...notifications,
+			if (to === address) {
+				const objIndex = notifications.findIndex(
+					(obj) => obj.userAddress === address
+				);
+				if (objIndex !== -1) {
+					const updatedNotifications = [...notifications];
+					updatedNotifications[objIndex].notification.push(
+						`${numeral(formatUnit(value)).format(
+							","
+						)} DAI was funded in your account`
+					);
+					setNotifications(updatedNotifications);
+					console.log(
+						"updatedNotifications in use contract event",
+						updatedNotifications
+					);
+					console.log('objIndex !== -1 is working')
+				}
+				console.log(
 					`${numeral(formatUnit(value)).format(
 						","
 					)} DAI was funded in your account`
-				]);
+				);
+			}
 		}
 	});
 
@@ -169,7 +190,9 @@ const NotificationBell = () => {
 			if (address === freelancer) {
 				setNotifications([
 					...notifications,
-					`You received ${numeral(formatUnit(amount)).format(",")} DAI for a job`
+					`You received ${numeral(formatUnit(amount)).format(
+						","
+					)} DAI for a job`
 				]);
 			}
 			console.log(
@@ -224,3 +247,230 @@ const NotificationBell = () => {
 };
 
 export default NotificationBell;
+
+// import NotificationItem from ".";
+// import { useState } from "react";
+// import NotificationPopOver from "./notification-popOver";
+
+// // images
+// import bellIcon from "@/public/asset/icons/bell-icon.svg";
+// import { useAccount, useContractEvent } from "wagmi";
+// import {
+// 	DaiContractAbi,
+// 	DiaContractAddress,
+// 	formatUnit,
+// 	formatWalletAddress,
+// 	GigzaContractAbi,
+// 	GigzaContractAddress
+// } from "utils/helper";
+// import numeral from "numeral";
+
+// // const useGetPersistedStore = () => {
+// // 	if (
+// // 		typeof window !== "undefined" &&
+// // 		localStorage.getItem("persist-gigza-notification") !== "undefined"
+// // 	) {
+// // 		// @ts-ignore
+// // 		JSON.parse(localStorage.getItem("persist-gigza-notification")) || [];
+// // 	}
+// // };
+
+// const NotificationBell = () => {
+// 	const [showNotification, setShowNotification] = useState(false);
+// 	const [notifications, setNotifications] = useState<string[]>([]);
+// 	const { address } = useAccount();
+// 	const handleClick = () => setShowNotification(!showNotification);
+
+// 	// ------------------------------ Transfer event (mint DAI tokens) ----------------------------
+
+// 	useContractEvent({
+// 		address: DiaContractAddress,
+// 		abi: DaiContractAbi,
+// 		eventName: "Transfer",
+// 		listener(from, to, value) {
+// 			if (to === address)
+// 				setNotifications([
+// 					...notifications,
+// 					`${numeral(formatUnit(value)).format(
+// 						","
+// 					)} DAI was funded in your account`
+// 				]);
+// 		}
+// 	});
+
+// 	// ------------------------------ ProfileCreated event ----------------------------
+
+// 	useContractEvent({
+// 		address: GigzaContractAddress,
+// 		abi: GigzaContractAbi,
+// 		eventName: "ProfileCreated",
+// 		listener(user, name, skill) {
+// 			if (user === address) {
+// 				setNotifications([...notifications, "Your profile was created"]);
+// 				console.log(`${user} - ${name} - ${skill}`);
+// 			}
+// 		}
+// 	});
+
+// 	// ------------------------------ JobPosted event ----------------------------
+
+// 	useContractEvent({
+// 		address: GigzaContractAddress,
+// 		abi: GigzaContractAbi,
+// 		eventName: "JobPosted",
+// 		listener(jobId, title, amount, description, timeline, client) {
+// 			if (client === address) {
+// 				setNotifications([...notifications, `You posted a job`]);
+// 				console.log(
+// 					`jobId: ${jobId} - title: ${title} - amount: ${amount} description: ${description} timeline: ${timeline} client: ${client}`
+// 				);
+// 			} else {
+// 				// @ts-ignore
+// 				setNotifications([
+// 					...notifications,
+// 					// @ts-ignore
+// 					`${formatWalletAddress(client)} posted a job`
+// 				]);
+// 			}
+// 		}
+// 	});
+
+// 	// ------------------------------ ProposalSubmitted  event ----------------------------
+
+// 	useContractEvent({
+// 		address: GigzaContractAddress,
+// 		abi: GigzaContractAbi,
+// 		eventName: "ProposalSubmitted",
+// 		listener(jobId, description, client) {
+// 			if (address === client) {
+// 				setNotifications([...notifications, "You submitted a job proposal"]);
+// 			}
+// 			// console.log(
+// 			// 	`jobId: ${jobId} - description: ${description}  client: ${client}`
+// 			// );
+// 		}
+// 	});
+
+// 	// ------------------------------ ContractSent  event ----------------------------
+
+// 	useContractEvent({
+// 		address: GigzaContractAddress,
+// 		abi: GigzaContractAbi,
+// 		eventName: "ContractSent",
+// 		listener(jobId, freelancer) {
+// 			if (address === freelancer) {
+// 				setNotifications([...notifications, "A contract has been sent to you"]);
+// 			}
+// 			// console.log(`jobId: ${jobId} - freelancer: ${freelancer}`);
+// 		}
+// 	});
+
+// 	// ------------------------------ ContractAccepted  event ----------------------------
+
+// 	useContractEvent({
+// 		address: GigzaContractAddress,
+// 		abi: GigzaContractAbi,
+// 		eventName: "ContractAccepted",
+// 		listener(jobId, freelancer) {
+// 			if (address === freelancer) {
+// 				setNotifications([...notifications, "You have accepted a contract"]);
+// 			}
+// 			// console.log(`jobId: ${jobId} - freelancer: ${freelancer}`);
+// 		}
+// 	});
+
+// 	// ------------------------------ OfferDeclined  event ----------------------------
+
+// 	useContractEvent({
+// 		address: GigzaContractAddress,
+// 		abi: GigzaContractAbi,
+// 		eventName: "OfferDeclined",
+// 		listener(jobId, amount, title) {
+// 			// if (address === freelancer) {
+// 			// 	setNotifications([...notifications, "You have accepted a contract"]);
+// 			// }
+// 			console.log(`jobId: ${jobId} - amount: ${amount} - title: ${title}`);
+// 		}
+// 	});
+
+// 	// ------------------------------ JobCompleted  event ----------------------------
+
+// 	useContractEvent({
+// 		address: GigzaContractAddress,
+// 		abi: GigzaContractAbi,
+// 		eventName: "JobCompleted",
+// 		listener(jobId, message, freelancer) {
+// 			if (address === freelancer) {
+// 				setNotifications([...notifications, "You have submitted a job"]);
+// 			}
+// 			// console.log(
+// 			// 	`jobId: ${jobId} - message: ${message} - freelancer: ${freelancer}`
+// 			// );
+// 		}
+// 	});
+
+// 	// ------------------------------ PayementReleased  event ----------------------------
+
+// 	useContractEvent({
+// 		address: GigzaContractAddress,
+// 		abi: GigzaContractAbi,
+// 		eventName: "PayementReleased",
+// 		listener(jobId, amount, freelancer) {
+// 			if (address === freelancer) {
+// 				setNotifications([
+// 					...notifications,
+// 					`You received ${numeral(formatUnit(amount)).format(",")} DAI for a job`
+// 				]);
+// 			}
+// 			console.log(
+// 				`jobId: ${jobId} - amount: ${amount} - freelancer: ${freelancer}`
+// 			);
+// 		}
+// 	});
+
+// 	// ------------------------------ ReviewCreated  event ----------------------------
+
+// 	useContractEvent({
+// 		address: GigzaContractAddress,
+// 		abi: GigzaContractAbi,
+// 		eventName: "ReviewCreated",
+// 		listener(client, message, rating) {
+// 			if (address === client) {
+// 				setNotifications([
+// 					...notifications,
+// 					`You recieved a ${rating} for a job`
+// 				]);
+// 			}
+// 			console.log(
+// 				`client: ${client} - message: ${message} - rating: ${rating}`
+// 			);
+// 		}
+// 	});
+
+// 	// useEffect(() => {
+// 	// 	localStorage.setItem(
+// 	// 		"persist-gigza-notification",
+// 	// 		JSON.stringify(notifications)
+// 	// 	);
+// 	// 	// }
+// 	// 	//eslint-disable-next-line
+// 	// }, [notifications]);
+
+// 	return (
+// 		<>
+// 			<NotificationItem
+// 				type="notification"
+// 				icon={bellIcon}
+// 				isActive={!!notifications?.length}
+// 				{...{ handleClick }}
+// 			/>
+// 			{showNotification ? (
+// 				<NotificationPopOver
+// 					{...{ notifications, setShowNotification, showNotification }}
+// 				/>
+// 			) : null}
+// 		</>
+// 	);
+// };
+
+// export default NotificationBell;
--- utils/helper/store.helper.ts
+++ utils/helper/store.helper.ts
@@ -1,11 +1,25 @@
 import GigzaContractAbi from "../constants/Gigza.json";
 import DaiContractAbi from "../constants/Dia.json";
+import { useEffect } from "react";
+import { getAccount } from "@wagmi/core";
 
 // new proxy
 const GigzaContractAddress = "0x81D4523ECC0655929248efe220bcBB805030b79d";
 // old
 // const GigzaContractAddress = "0x6B042519F64CDf02b9D0c26885662434032bF178";
 const DiaContractAddress = "0x40fb4204dDe488f34b9d9E0056d0FE8f6ab38585";
+
+// const { address } = getAccount();
+
+// const initialNotification: {
+// 	userAddress: `0x${string}` | undefined;
+// 	notification: string[];
+// }[] = [
+// 	{
+// 		userAddress: address,
+// 		notification: []
+// 	}
+// ];
 
 const bidState = [
 	"sent",
@@ -33,6 +47,84 @@ const bidStatus = {
 	// 5: "Paid",
 	6: "Indispute"
 };
+
+// const useSetPersistNotificationStore = (store: string[]) => {
+// 	useEffect(() => {
+// 		localStorage.setItem("persist-gigza-notification", JSON.stringify(store));
+// 	// eslint-disable-next-line react-hooks/exhaustive-deps
+// 	}, [store, address]);
+// };
+
+// const useGetPersistedStore = () =>
+// 	JSON.parse(localStorage.getItem("persist-gigza-notification")!) ||
+// 	initialNotification;
+
+type Notification = {
+	userAddress: `0x${string}` | undefined;
+	notification: string[];
+}[];
+
+const useSetPersistNotificationStore = (
+	store: Notification,
+	address: `0x${string}`
+) => {
+	useEffect(() => {
+		console.log("store", store);
+		console.log('address',address)
+		let userNotificationData = []
+		// retrieve existing notification data from local storage
+		let existingNotification: Notification =
+			JSON.parse(localStorage.getItem("persist-gigza-notification")!) || store;
+
+			const objIndexOfUserNotification = existingNotification.findIndex((obj) => obj.userAddress === address)
+			const userNoticationIndexInStore = store.findIndex((obj) => obj.userAddress === address)
+
+			if (objIndexOfUserNotification !== -1 && userNoticationIndexInStore !== -1) {
+				existingNotification[objIndexOfUserNotification].notification.push(...store[userNoticationIndexInStore].notification)
+			}
+			
+
+		// // check if the user address is in existingNotification
+		// let userNotification = existingNotification.filter(
+		// 	(item) => item.userAddress === address
+		// );
+		// console.log("userNotification", userNotification);
+
+		// if (userNotification.length) {
+		// 	const objIndex = store.findIndex(
+		// 		(obj) => obj.userAddress === address
+		// 	);
+		// 	if (objIndex !== -1) {
+		// 		const updatedUserNotification = store[objIndex]
+		// 		userNotification[0].notification.push(...updatedUserNotification.notification)
+				
+		// 	}
+		// 	return;
+		// }
+		// add new user to existing data
+		let newUserNotification = address!
+			? {
+					userAddress: address,
+					notification: []
+			  }
+			: [];
+		let updatedNotification =
+			// @ts-ignore
+			existingNotification.length || newUserNotification.userAddress
+				? [...existingNotification, newUserNotification,]
+				: [];
+
+		localStorage.setItem(
+			"persist-gigza-notification",
+			JSON.stringify(updatedNotification)
+		);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [store, address]);
+};
+
+const useGetPersistedNotificationStore = () =>
+	JSON.parse(localStorage.getItem("persist-gigza-notification")!) || [];
+
 export {
 	// initialState,
 	// useSetPersistStore,
@@ -43,5 +135,7 @@ export {
 	DaiContractAbi,
 	bidState,
 	jobState,
-	bidStatus
+	bidStatus,
+	useSetPersistNotificationStore,
+	useGetPersistedNotificationStore
 };
